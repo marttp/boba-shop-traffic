ea849f1c3da051c6704f0ac40ea534e8f8d68732
Thanaphoom Babparn <tp.babparn@gmail.com>
2022-05-14 20:08:56 +0700

    Add Order Service Test

--- boba-shop/src/test/kotlin/dev/tpcoder/bobashop/service/DeleteSchedulerTest.kt
+++ boba-shop/src/test/kotlin/dev/tpcoder/bobashop/service/DeleteSchedulerTest.kt
@@ -1,4 +1,23 @@
 package dev.tpcoder.bobashop.service
 
+import dev.tpcoder.bobashop.repository.*
+import io.mockk.mockk
+
 internal class DeleteSchedulerTest {
+
+    private val menuRepository: MenuRepository = mockk()
+    private val orderRepository: OrderRepository = mockk()
+    private val popularRepository: PopularRepository = mockk()
+    private val incomeRepository: IncomeRepository = mockk()
+    private val orderMenuRepository: OrderMenuRepository = mockk()
+    private val menuCounterRepository: MenuCounterRepository = mockk()
+
+    private val deleteScheduler: DeleteScheduler = DeleteScheduler(
+        menuRepository,
+        orderRepository,
+        popularRepository,
+        incomeRepository,
+        orderMenuRepository,
+        menuCounterRepository
+    )
 }
--- boba-shop/src/test/kotlin/dev/tpcoder/bobashop/service/OrderServiceTest.kt
+++ boba-shop/src/test/kotlin/dev/tpcoder/bobashop/service/OrderServiceTest.kt
@@ -1,3 +1,63 @@
 package dev.tpcoder.bobashop.service
 
-internal class OrderServiceTest {}
+import dev.tpcoder.bobashop.model.Menu
+import dev.tpcoder.bobashop.model.Order
+import dev.tpcoder.bobashop.model.OrderMenu
+import dev.tpcoder.bobashop.model.dto.MenuWithOption
+import dev.tpcoder.bobashop.repository.OrderMenuRepository
+import dev.tpcoder.bobashop.repository.OrderRepository
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import java.math.BigDecimal
+import java.time.ZonedDateTime
+
+internal class OrderServiceTest {
+
+    private val orderRepository: OrderRepository = mockk()
+    private val orderMenuRepository: OrderMenuRepository = mockk()
+    private val orderService: OrderService = OrderService(orderRepository, orderMenuRepository)
+
+    @Test
+    @DisplayName("Order Ingress - Success")
+    fun whenOrderIncoming_returnSuccess() {
+
+    }
+
+    @Test
+    @DisplayName("Order limitation exceed")
+    fun whenOrderIncomingTooMuch_returnError() {
+
+    }
+
+    @Test
+    @DisplayName("Add order")
+    fun whenAddOrder_returnSuccess() {
+        val mockOrder = Order(
+            id = 1,
+            orderDate = ZonedDateTime.now(),
+            userId = "user1"
+        )
+        every { orderRepository.save(any()) } returns mockOrder
+        val mockMenu = Menu(
+            id = 1
+        )
+        val orderMenu = OrderMenu(
+            order = mockOrder,
+            menu = mockMenu,
+            amount = 2
+        )
+        val afterOrderMenu = OrderMenu(
+            id = 1,
+            order = mockOrder,
+            menu = mockMenu,
+            amount = 2
+        )
+        every { orderMenuRepository.saveAll(listOf(orderMenu)) } returns listOf(afterOrderMenu)
+        orderService.addOrder(mockOrder, listOf(MenuWithOption(mockMenu.id!!, 2)))
+        verify(exactly = 1) { orderRepository.save(any()) }
+        verify(exactly = 1) { orderMenuRepository.saveAll(listOf(orderMenu)) }
+    }
+}
